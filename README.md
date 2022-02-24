39eb25db831d8ced2fb3c44e0a5fab1b2dda1432
sw7999 <542983223@qq.com>
2022-02-24 23:06:05 +0800

    extract  github config from code to properties

---
+++ README.md
@@ -0,0 +1,11 @@
+## 孤勇者论坛
+
+github上，主要写的一个项目
+##资料
+https://spring.io/guides
+https://spring.io/guides/gs/serving-web-content
+
+https://docs.github.com/cn/developers/apps/building-oauth-apps/scopes-for-oauth-apps
+
+## 工具
+https://git-scm.com/download
---
+++ src/main/java/life/suwei/community2/controller/AuthorizeController.java
@@ -0,0 +1,46 @@
+package life.suwei.community2.controller;
+
+import life.suwei.community2.dto.AccessTokenDTO;
+import life.suwei.community2.dto.GithubUser;
+import life.suwei.community2.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author shkstart
+ * @date 2022/2/24-21:25
+ */
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String client_secret;
+    @Value("${github.redirect.uri}")
+    private String redirect_uri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code")String code,
+                           @RequestParam(name = "state")String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(client_secret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirect_uri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+
+
+}
